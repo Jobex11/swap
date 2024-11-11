9e052422a3eb9a293648e5b24c89d935d334e5c2
jobex11 <joblawal33@gmail.com>
2024-11-11 10:33:38 +0100

    added swap view

--- swapfrontend/src/App.jsx
+++ swapfrontend/src/App.jsx
@@ -1,39 +1,60 @@
 function App() {
   return (
-    <>
+    <div>
       <div>
-        <div className="content-box">
-          <div className="swap-box">
-            <div className="title">
-              <div>SWAP</div>
-              <div className="pro">PRO</div>
-            </div>
-            <div className="checker">
-              <h3>mark checker</h3>
-              <h3>Swapped!</h3>
-            </div>
-            {/* swap pay*/}
-            <div className="pay">
-              <h3>you pay</h3>
-              <input></input>
-              <div>image here</div>
-            </div>
-            {/* swap  receive*/}
-            <div className="receive">
-              <h3>you receives</h3>
-              <h3>0.069451</h3>
+        {/* add*/}
 
-              <div>image here</div>
+        <div>
+          <button>connect wallet</button>
+        </div>
+        <div>
+          <span>Wallet Addr:</span>
+          <span>abs...</span>
+        </div>
+        <div>
+          <span>Swap token Amount:</span>
+          <span>23 optimus</span>
+        </div>
+      </div>
+      <div className="content-box">
+        <div className="swap-box">
+          <div className="title">
+            <div>SWAP</div>
+            <div className="pro">PRO</div>
+          </div>
+          <hr className="border-t border-gray-500 w-full my-3" />
+          <div className="checker">
+            <h5>mark checker</h5>
+            <h3>Swapped!</h3>
+          </div>
+          {/* swap pay*/}
+          <div className="bg-[#3d3b3b] rounded-lg my-3 p-4 w-full">
+            <h4>You Pay</h4>
+            <div>
+              <input
+                placeholder="0"
+                className="bg-[#3d3b3b] py-2 text-lg my-1"
+              />
             </div>
-            {/* swap button */}
-            <div>View transaction</div>
-            <div className="swap">
-              <button>Swap, swapping, insufficient funds, done</button>
-            </div>
+            <h4>50K 100k 250k 1M Max</h4>
+          </div>
+          {/* swap  receive*/}
+          <div className="bg-[#3d3b3b] rounded-lg my-3 p-4 w-full">
+            <h3>you receives</h3>
+            <h3 className="py-2 text-lg my-1">0</h3>
+            <h3 className="text-end">LiQ: 68.68M</h3>
+          </div>
+          {/* swap button */}
+          <div>View transaction</div>
+          <div>
+            Price Protection <span>[span]</span>
+          </div>
+          <div className="swap">
+            <button>Swap, swapping, insufficient funds, done</button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
